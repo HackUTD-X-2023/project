d254def4513c72f58527d90973783fa1ed03dab3
Bhavesh Mandalapu <mandalapubhavesh@gmail.com>
2023-11-04 17:54:54 -0500

    Blur

--- app/page.js
+++ app/page.js
@@ -5,7 +5,7 @@ import { useRouter } from 'next/navigation'
 export default function Home() {
   const router = useRouter();
   return (
-    <main className="bg-family h-screen bg-no-repeat bg-cover relative -z-10">
+    <main className="bg-family blur-lg h-screen bg-no-repeat bg-cover relative -z-10">
       <div className="h-full w-full backdrop-blur-sm bg-black opacity-60 z-10 relative">
         <div className="bg-black z-20 absolute">
           <button className="bg-sky z-30" type="button" onClick={() => router.push('/form')}>
