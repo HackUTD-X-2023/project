780a7990b4fbbb08aa2c70cc988d078944d0ddb3
Rohit N <rohitnair@MacBook-Airy.local>
2023-11-04 17:13:54 -0500

    Added bg image

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,9 +11,11 @@ module.exports = {
         work: ["Work", "sans"]
       },
       backgroundImage: {
+        'family': "url('/cover.jpg')",
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        
       },
     },
   },
