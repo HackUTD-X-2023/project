0dd32ce6cf4f53483e2b1a6925ecc3fdaa00985b
Bhavesh Mandalapu <mandalapubhavesh@gmail.com>
2023-11-04 21:13:55 -0500

    Style fix for anshuman

--- app/page.js
+++ app/page.js
@@ -6,11 +6,13 @@ export default function Home() {
   const router = useRouter();
   return (
     <main className="bg-[url('/cover.jpg')] h-screen bg-no-repeat bg-cover relative z-10 flex flex-col justify-center">
-      <div className='mt-40 ml-52 px-16 py-8 bg-gray-800 w-fit rounded-2xl flex flex-col gap-y-4'>
-        <div className='text-white text-2xl max-w-xs'>Find out if you can buy a home in minutes, <br />super-powered with AI</div>
-        <button className="rounded-2xl text-lg text-white bg-sky-400 h-16 w-64" type="button" onClick={() => router.push('/form')}>
-          GET STARTED!
-        </button>
+      <div className='mt-40 ml-52 px-16 py-8 bg-gray-800 w-fit rounded-3xl '>
+        <div className='my-10 flex flex-col gap-y-10 items-center'>
+          <div className='text-white text-3xl max-w-sm font-medium leading-8'>Find out if you can buy a home in minutes, <br />super-powered with AI</div>
+          <button className="rounded-2xl text-lg text-white bg-sky-400 h-16 w-64" type="button" onClick={() => router.push('/form')}>
+            GET STARTED!
+          </button>
+        </div>
       </div>
     </main>
   )
